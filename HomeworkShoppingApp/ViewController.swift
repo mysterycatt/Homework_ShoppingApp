bc6834e09a6d9c66531c6aa43ee14da06d9eaa0f
Pei Wu <peiwu@2001-b011-c001-1171-0892-7192-e162-b216.dynamic-ip6.hinet.net>
2018-08-19 16:11:57 +0800

    Initial Commit

---
+++ HomeworkShoppingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeworkShoppingApp
+//
+//  Created by Pei Wu on 2018/8/19.
+//  Copyright © 2018年 Pei Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
